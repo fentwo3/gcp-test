a9ed7d609cf38efbb8b2284411dfbfc85754757b
t7abhay <abhaytaras18@gmail.com>
2025-02-06 14:41:56 +0530

    dsd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
